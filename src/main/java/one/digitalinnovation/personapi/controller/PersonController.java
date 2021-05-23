09cbbae487613d47e0dcdd7f390c01545cdfd427
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-05-23 16:59:35 -0300

    Criado o pacote service e a  Classe PersonService

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -3,28 +3,24 @@ package one.digitalinnovation.personapi.controller;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
 import one.digitalinnovation.personapi.repository.PersonRepository;
+import one.digitalinnovation.personapi.service.PersonService;
 import org.aspectj.bridge.Message;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    private PersonRepository personRepository;
+    private PersonService personService;
 
-    public PersonController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+    public PersonController(PersonService personService) {
+        this.personService = personService;
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public MessageResponseDTO createPerson(@RequestBody Person person){
-        Person savedperson = personRepository.save(person);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID " + savedperson.getId())
-                .build();
+        return personService.createPerson(person);
     }
 }
