fd764b329e3046efee1adbcab79093dc9aa2c12c
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-05-24 00:03:08 -0300

    Aprendendo sobre a Biblioteca MapStruct que serve para converter Entity para DTO ou vice-versa e tambem criado o metodo getAll() para Classe Person utilizando streams

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -3,24 +3,39 @@ package one.digitalinnovation.personapi.service;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
 
     private PersonRepository personRepository;
 
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
     public MessageResponseDTO createPerson(@RequestBody PersonDTO personDTO){
-        PersonDTO savedperson = personRepository.save(personDTO);
+        Person personToSave = personMapper.toModel(personDTO);
+        Person savedperson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Created person with ID " + savedperson.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll(){
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(people -> {
+                    return personMapper.toDTO(people);
+                }).collect(Collectors.toList());
     }
 }
