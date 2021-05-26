bd7ddbe2c5e09f48e1788c240b6e058c8e848ea1
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-05-26 00:30:21 -0300

    Adicionado as configurações do Swagger para Documentar a API

--- src/main/java/one/digitalinnovation/personapi/dto/request/PersonDTO.java
+++ src/main/java/one/digitalinnovation/personapi/dto/request/PersonDTO.java
@@ -1,5 +1,6 @@
 package one.digitalinnovation.personapi.dto.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,23 +18,29 @@ import java.util.List;
 @NoArgsConstructor
 public class PersonDTO {
 
+    @ApiModelProperty(name = "id", value = "ID", example = "1", position = 0)
     private Long id;
 
     @NotEmpty
     @Size(min = 2, max = 100)
+    @ApiModelProperty(name = "firstName", value = "Primeiro nome", example = "James", position = 1)
     private String firstName;
 
     @NotEmpty
     @Size(min = 2, max = 100)
+    @ApiModelProperty(name = "lastName", value = "Ultimo nome", example = "Gosling", position = 2)
     private String lastName;
 
     @NotEmpty
     @CPF
+    @ApiModelProperty(name = "cpf", value = "CPF", example = "42090734078", position = 3)
     private String cpf;
 
+    @ApiModelProperty(name = "birthDate", value = "Data de Nascimento", example = "04-04-2010", position = 4)
     private String birthDate;
 
     @NotEmpty
     @Valid
+    @ApiModelProperty(name = "phones", value = "Telefones", position = 5)
     private List<PhoneDTO> phones;
 }
